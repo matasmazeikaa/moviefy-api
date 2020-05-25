d742e2006a702bf027b23c0febde6412360be213
Matas Mazeika <matasmazeika@Matass-MBP.lan>
2020-05-25 17:56:28 +0300

    Added error messages and gitignore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sort = require('fast-sort');
 const movies = require('./movies.json');
+
 const server = express();
 
 const port = process.env.PORT || 3001;
@@ -13,20 +14,27 @@ server.use((req, res, next) => {
 server.listen(port, () => console.log(`http://localhost:${port}`));
 
 server.get('/movies', (req, res) => {
-    const sortedMovies = sort(movies)[req.query.order]((movie) => movie[req.query.sort_by]);
+    if (!req.query.order || !req.query.page || !req.query.limit || !req.query.sort_by) {
+        res.status(400).json({ message: 'Missing query params' });
+    }
 
-    const page = req.query.page;
-    const limit = req.query.limit
-    const movieCount = sortedMovies.length;
+    try {
+        const sortedMovies = sort(movies)[req.query.order]((movie) => movie[req.query.sort_by]);
+        const page = req.query.page;
+        const limit = req.query.limit
+        const movieCount = sortedMovies.length;
 
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
 
-    const list = sortedMovies.slice(startIndex, endIndex);
+        const list = sortedMovies.slice(startIndex, endIndex);
 
-    res.json({
-        count: movieCount,
-        list,
-    });
+        res.json({
+            count: movieCount,
+            list,
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Something went wrong, please try refreshing the page' });
+    }
 });
 
